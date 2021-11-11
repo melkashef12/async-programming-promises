258c760f0ec895f1e2f11e5af90fef5107114c8e
Mohamed El Kashef <melkashef12@gmail.com>
2021-11-11 08:36:50 +0100

    [02 - Consuming] Performing one last operation

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -31,4 +31,17 @@ export function chainCatch() {
 }
 
 export function final() {
+    showWaiting();
+    axios.get("http://localhost:3000/orders/1")
+        .then(({data}) => {
+            return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`)
+        })
+        .then(({data}) => {
+            setText(`City: ${data.city}`);
+        })
+        .catch(err => setText(err))
+        .finally( () => {
+            setTimeout(()=> hideWaiting(),1500);
+            appendText('--- Completely Done');
+        });
 }
